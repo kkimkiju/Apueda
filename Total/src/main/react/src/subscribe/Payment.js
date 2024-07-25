0b299059a2e9525c45e6d4bb9b48c805ba9a2fdf
kimfjd <kimfjd@naver.com>
2024-07-25 11:57:55 +0900

    푸쉬

--- Total/src/main/react/src/subscribe/Payment.js
+++ Total/src/main/react/src/subscribe/Payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PaymentApi from "../api/PaymentAxios";
 import axios from "axios";
 import styled from "styled-components";
